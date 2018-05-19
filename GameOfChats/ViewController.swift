c2f81b30766d4480be5979a107934f10be4d3a6c
Shaun Tucker <tucke@shauns-macbook-air.local>
2018-05-19 11:13:33 -0400

    Initial Commit

---
+++ GameOfChats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfChats
+//
+//  Created by Shaun Tucker on 5/19/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
