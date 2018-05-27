0db76c66cda380931ad76906fd17d0dfcfb132d7
Shaun Tucker <tucke@cpe-2606-a000-1319-816a-0-b056-6bfd-a02f.dyn6.twc.com>
2018-05-27 09:50:23 -0400

    check if user is logged in (success)

--- GameOfChats/Controller/MessageController.swift
+++ GameOfChats/Controller/MessageController.swift
@@ -9,16 +9,29 @@
 import UIKit
 import Firebase
 
-class ViewController: UITableViewController {
+class MessageController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
+        checkIfuserIsLoggedIn()
+        
+      
+    }
+    
+    func checkIfuserIsLoggedIn() {
+        
         //User not logged in
         if Auth.auth().currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
+            
+        } else {
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observe(.value, with: { (snapshot) in
+                print(snapshot)
+            }, withCancel: nil)
             
         }
     }
