33b907d8c52b693f5fbb2ab9c26fb83ac9d92076
Shaun Tucker <tucke@shauns-air.home>
2018-05-27 11:53:10 -0400

    fetchUsers

--- GameOfChats/Controller/NewMessageController.swift
+++ GameOfChats/Controller/NewMessageController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
 class NewMessageController: UITableViewController {
 
+    let cellid = "cellid"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        fetchUser()
         
     }
+    //MARK: - VERY IMPORTANT!!!
+    func fetchUser() {
+        
+        Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            print(snapshot)
+        }, withCancel: nil)
+    }
 
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
 
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellid)
+        
+        cell.textLabel?.text = "Shawn will succeed"
+        
+        return cell
+    }
 
 }
